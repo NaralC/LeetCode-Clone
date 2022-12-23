cede2e65c9991971bbd61f7119c9d7af165688bc
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-23 21:42:54 +0000

    init React + TS + Tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-white w-screen h-screen flex flex-col justify-center items-center">
+      
+    </div>
+  )
+}
+
+export default App
