f69bef97d86ccc77ab7468a33662bf99465fd3be
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-23 22:25:40 +0000

    init express backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
   }, []);
 
   const handleSubmit = (): void => {
-    return;
+    console.log(code);
   }
 
   return (
@@ -29,7 +29,7 @@ const App: React.FC = () => {
         onChange={onChange}
         theme={okaidia}
       />
-      <button onClick={handleSubmit} className='border-2 bg-green-600 p-2 text-white font-medium'>Submit</button>
+      <button onClick={handleSubmit} className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>Submit</button>
     </div>
   );
 };
