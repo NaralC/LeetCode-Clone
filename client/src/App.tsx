5c43f5961717a660a196530d476dbcbf93412367
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-23 21:55:58 +0000

    add CodeMirror (js, py)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { useCallback } from "react";
+import { okaidia } from "@uiw/codemirror-theme-okaidia";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const code = "const a = 0;";
+
+  const onChange = useCallback((value: any, viewUpdate: any) => {
+    console.log("value:", value);
+  }, []);
 
   return (
-    <div className="bg-white w-screen h-screen flex flex-col justify-center items-center">
-      
+    <div className="bg-grey-200 w-screen h-screen flex flex-col justify-center">
+      <CodeMirror
+        value={code}
+        height="300px"
+        // extensions={[javascript({ jsx: true, typescript: true })]}
+        extensions={[python()]}
+        onChange={onChange}
+        theme={okaidia}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
